e0c508609fff594be5d0b668bdb5ca4c569b3164
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2026-04-28 19:25:14 +0530

    v2.0

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.a4n5an7elt"
+    "revision": "0.96s6au38q2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
