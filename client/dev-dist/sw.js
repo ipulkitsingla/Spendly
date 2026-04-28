208771b098bfe3377cff99640cf3447bec55555a
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2026-04-28 20:17:29 +0530

    health

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.a4n5an7elt"
+    "revision": "0.f739d2o8qd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
